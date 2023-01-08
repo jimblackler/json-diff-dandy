1d74fa79e2e845b549283013b2b77394c90feb56
jimblackler <jimblackler@gmail.com>
2023-01-08 22:53:41 +0000

    Added two new methods.

--- lib/diffDandy.compare.test.ts
+++ lib/diffDandy.compare.test.ts
@@ -1,8 +1,11 @@
 import {compare, GetOperation} from "fast-json-patch";
-import {diff} from 'json8-patch';
+import diff from 'json-patch-gen';
+import {diff as diff1} from 'json8-patch';
 import {createPatch} from "rfc6902";
 import {assertNotNull} from "./check/null";
 import {JSONPatchOperation, JSONValue} from "./jsonTypes";
+
+const jiff = require('jiff');
 
 type Technique = {
   name: string;
@@ -24,9 +27,18 @@ describe('diffDandy.compare.test', () => {
   tests.forEach(test =>
       describe(`${JSON.stringify(test.doc1)} -> ${JSON.stringify(test.doc2)}`, () => {
         const techniques: Technique[] = [
-          {name: 'JSON8', getDiff: (a, b) => diff(a, b)},
+          {name: 'JSON8', getDiff: (a, b) => diff1(a, b)},
           {name: 'rfc6902', getDiff: (a, b) => createPatch(a, b)},
-          {name: 'fast-json-patch', getDiff: (a, b) => compare(assertNotNull(a), assertNotNull(b))}
+          {name: 'fast-json-patch', getDiff: (a, b) => compare(assertNotNull(a), assertNotNull(b))},
+          {name: 'jiff', getDiff: (a, b) => jiff.diff(a, b, undefined)},
+          {
+            name: 'json-patch-gen', getDiff: (a, b) => {
+              if (typeof a === 'object' && typeof b === 'object') {
+                return diff(a, b);
+              }
+              return null;
+            }
+          }
         ];
         techniques.forEach(technique => {
           it(technique.name, () => {
