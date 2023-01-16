4910030b16d77c94d292a890cb97f4a691949a93
Jim Blackler <jimblackler@gmail.com>
2023-01-16 19:37:58 +0000

    Minor fix.

--- lib/diffDandy.ts
+++ lib/diffDandy.ts
@@ -146,7 +146,7 @@ export function diff(original: JSONValue, target: JSONValue): JSONPatchOperation
                 let sequenceNumber2 = sequenceNumber;
                 let huntIdx = existingIdx + 1;
                 while (huntIdx < value.length) {
-                  if (sequence[sequenceNumber2][1] == huntIdx) {
+                  if (sequence[sequenceNumber2][1] === huntIdx) {
                     sequenceNumber2++;
                   } else if (isEqual(existing[existingIdx], value[huntIdx])) {
                     break;
